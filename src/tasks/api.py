2d5b9339938d7bbdb60a76b097ce16ccdd5f9971
Alberto Casero <alberto@kasfactory.net>
2017-03-09 21:15:29 +0100

    Implementamos los endpoints de detalle, actualicion y borrado de una tarea usando un Generic API View

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
@@ -8,6 +8,14 @@ from tasks.serializers import TaskSerializer
 class TasksAPI(ListCreateAPIView):
     """
     Lists (GET) and creates (POST) Tasks
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetailAPI(RetrieveUpdateDestroyAPIView):
+    """
+    Retrieves (GET), updates (PUT) and destroy (DELETE) a given Task
     """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
--- src/todoro/urls.py
+++ src/todoro/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from tasks.api import TasksAPI
+from tasks.api import TasksAPI, TaskDetailAPI
 from tasks.views import tasks_list, tasks_detail, NewTaskView
 from users.api import UsersAPI, UserDetailAPI
 from users.views import LoginView, logout
@@ -34,5 +34,6 @@ urlpatterns = [
     url(r'^api/1.0/users/(?P<pk>[0-9]+)/?$', UserDetailAPI.as_view(), name="user_detail_api"),
 
     # API Tasks
-    url(r'^api/1.0/tasks/$', TasksAPI.as_view(), name="tasks_api")
+    url(r'^api/1.0/tasks/$', TasksAPI.as_view(), name="tasks_api"),
+    url(r'^api/1.0/tasks/(?P<pk>[0-9]+)/?$', TaskDetailAPI.as_view(), name="task_detail_api")
 ]
